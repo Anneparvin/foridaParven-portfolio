15f601c72a6ec2375560dc152e2f9ba3b736ad65
Anneparvin <108586988+Anneparvin@users.noreply.github.com>
2023-08-28 01:56:37 +0600

    add6

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import AboutMe from "../AboutMe/AboutMe";
 import Contact from "../Contact/Contact";
 import FeaturedPro from "../FeaturedPro/FeaturedPro";
 import Logo from "../Logo/Logo";
@@ -12,6 +13,7 @@ const Home = () => {
            <Logo></Logo>
            <Services></Services> 
            <FeaturedPro></FeaturedPro>
+           <AboutMe></AboutMe>
            <Contact></Contact>
         </div>
     );
